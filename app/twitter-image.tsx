a04217ffb0d255a8bc750588ae6cd52ef34f190a
Elian <munelian@gmail.com>
2023-08-30 02:54:13 +0300

    modified open graph image

--- app/twitter-image.tsx
+++ app/twitter-image.tsx
@@ -1,44 +1,33 @@
-import { ImageResponse } from 'next/server'
+import { ImageResponse } from 'next/server';
+import 'tailwindcss/tailwind.css'; // Import Tailwind CSS
 
 // Route segment config
-export const runtime = 'edge'
+export const runtime = 'edge';
 
 // Image metadata
-export const alt = 'Web3 Club Africa'
+export const alt = 'Web3 Club Africa';
 export const size = {
     width: 1200,
     height: 630,
-}
+};
 
-export const contentType = 'image/png'
+export const contentType = 'image/png';
 
 // Image generation
 export default async function Image() {
-
-
     return new ImageResponse(
         (
             // ImageResponse JSX element
-            <div
-                style={{
-                    fontSize: 128,
-                    background: 'white',
-                    width: '100%',
-                    height: '100%',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-            >
-                Web3 Club Africa
+            <div className="w-full h-full bg-gradient-to-b from-blue-500 to-blue-700 flex items-center justify-center text-white p-10">
+                <div className="text-center">
+                    <h1 className="text-4xl font-bold mb-4">Web3 Club Africa</h1>
+                    <p className="text-2xl">Empowering Africa through Web3 Innovation</p>
+                </div>
             </div>
         ),
         // ImageResponse options
         {
-            // For convenience, we can re-use the exported opengraph-image
-            // size config to also set the ImageResponse's width and height.
             ...size,
-
         }
-    )
+    );
 }
