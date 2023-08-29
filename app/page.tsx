e664d86ae7678cc2b466bd7bdb5d98e0cc7083ea
Elian <munelian@gmail.com>
2023-08-30 01:23:37 +0300

    Optimized desc to 1000

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Web 3 Club Africa | HomePage',
-  description: 'Join the forefront of Web3 innovation in Africa. Explore boundless opportunities and conquer challenges within the dynamic Web3 landscape. Connect with visionaries and leaders within the African Web3 community.',
+  description: 'Embark on the forefront of Web3 innovation in Africa. Dive into limitless opportunities, overcoming dynamic challenges within the Web3 landscape. Network with visionary leaders in the African Web3 community.'
 }
 
 
